3004146a16911c6c546a7fca921c1a004c699ba5
VienNguyen113 <luongvien13t2@gmail.com>
2017-03-10 20:37:08 +0700

    scroll smooth

--- styles/custom/v1/js/main.js
+++ styles/custom/v1/js/main.js
@@ -23,4 +23,52 @@ $(document).ready(function() {
 		}
 	})
 
+	//Scroll
+	$('.linkScroll').on('click', function(e) {
+		e.preventDefault();
+		var scrollTarget = $(this).attr("data-target");
+		// document.querySelector("#"+scrollTarget).scrollIntoView();
+		$('html, body').animate({
+		    scrollTop: $("#"+scrollTarget).offset().top
+		}, 500);
+	})
+
+    showHeaderAndAsideFixed();
+    specifySection();
+
+	$(window).scroll(function(){
+        showHeaderAndAsideFixed();
+        specifySection();
+    });
+
 })
+
+function showHeaderAndAsideFixed() {
+    var window_top = $(window).scrollTop();
+    var about_top = $('#sectionAbout').offset().top;
+    if (window_top >= about_top) {
+        $('#headerFixed').show();
+        $('aside').show();
+    } else {
+        $('#headerFixed').hide();
+        $('aside').hide();
+    }
+}
+
+function specifySection() {
+    var window_top = $(window).scrollTop();
+    var about_top = $('#sectionAbout').offset().top;
+    var howItWork_top = $('#sectionHowItWork').offset().top;
+    var team_top = $('#sectionTeam').offset().top;
+    var getInTouch_top = $('#getInTouchForm').offset().top;
+
+    $('aside a').removeClass('active');
+
+    if (window_top >= about_top && window_top < howItWork_top) {
+    	$('aside a[data-target="sectionAbout"]').addClass('active');
+    } else if (window_top >= howItWork_top && window_top < team_top) {
+    	$('aside a[data-target="sectionHowItWork"]').addClass('active');
+    } else if (window_top >= team_top && window_top < getInTouch_top) {
+    	$('aside a[data-target="sectionTeam"]').addClass('active');
+    }
+}
